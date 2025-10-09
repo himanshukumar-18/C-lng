46da9253a25e9763dcf56c110b1fafc33c8ff6ba
Himanshu Kumar <rajh5343@gmail.com>
2025-10-09 17:44:24 +0530

    add some questions

--- switchCase/switch_case.c
+++ switchCase/switch_case.c
@@ -197,6 +197,7 @@ int main()
 
 // Write a C program using switch case to check whether a given character is a vowel or consonant. For non-alphabet characters, display “Invalid Input”.
 
+/*
 #include <stdio.h>
 
 int main()
@@ -236,3 +237,140 @@ int main()
         printf("Invalid input.");
     }
 }
+*/
+
+/*
+write a C program using switch case to display a menu for basic mathematical operations and perform the selected operation.
+Menu:
+    Addition
+    Subtraction
+Multiplication
+    Division
+    Modulus
+
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int select, x, y, output;
+
+    printf("1. Addition \n");
+    printf("2. Subtraction \n");
+    printf("3. Multiplication \n");
+    printf("4. Division \n");
+    printf("5. Modulus \n");
+
+    scanf("%d", &select);
+
+    switch (select)
+    {
+    case 1:
+        printf("You select Addition. \n");
+
+        printf("Enter the value of x & y: \n");
+        scanf("%d %d", &x, &y);
+
+        output = x + y;
+        printf("The addition of %d & %d = %d", x, y, output);
+
+        break;
+    case 2:
+        printf("You select Subtraction. \n");
+
+        printf("Enter the value of x & y: \n");
+        scanf("%d %d", &x, &y);
+
+        output = x - y;
+        printf("The subtraction of %d & %d = %d", x, y, output);
+
+        break;
+    case 3:
+        printf("You select Multiplication. \n");
+
+        printf("Enter the value of x & y: \n");
+        scanf("%d %d", &x, &y);
+
+        output = x * y;
+        printf("The multiplication of %d & %d = %d", x, y, output);
+
+        break;
+    case 4:
+        printf("You select Division. \n");
+
+        printf("Enter the value of x & y: \n");
+        scanf("%d %d", &x, &y);
+
+        output = x / y;
+        printf("The division of %d & %d = %d", x, y, output);
+
+        break;
+    case 5:
+        printf("You select Modulus. \n");
+
+        printf("Enter the value of x & y: \n");
+        scanf("%d %d", &x, &y);
+
+        output = x % y;
+        printf("The modulus of %d & %d = %d", x, y, output);
+
+        break;
+    }
+
+    default:
+        printf("Invalid choise.")
+}
+*/
+
+// ⁠ ⁠Write a C program using switch case to convert temperature based on the user’s choice.
+// Celsius to Fahrenheit
+// 	Fahrenheit to Celsius
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int userChoise;
+    float celsius, fahrenheit, output;
+
+    printf("1 for Celsius to Fahrenheit. \n");
+    printf("2 for Fahrenheit to Celsius. \n");
+
+    printf("What's your choise? \n");
+    scanf("%d", &userChoise);
+
+    switch (userChoise)
+    {
+    case 1:
+        printf("Your choise is Celsius to Fahrenheit. \n");
+
+        printf("Enter value of clesius \n");
+        scanf("%f", &celsius);
+
+        output = (celsius * 9 / 5) + 32;
+        printf("Result = %2.fFahrenheit.", output);
+
+        break;
+    case 2:
+        printf("Your choise is Fahrenheit to Clesius. \n");
+
+        printf("Enter value of Fahrenheit \n");
+        scanf("%f", &fahrenheit);
+
+        output = (fahrenheit - 32) * 5 / 9;
+        printf("Result = %2.fClesius.", output);
+
+        break;
+    default:
+        printf("Invalid Choise. \n");
+        break;
+    }
+}
+*/
+
+
+
+
