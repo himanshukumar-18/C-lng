70322e5d7ab699c9af1b9dc0b09e91b609edaf20
Himanshu Kumar <rajh5343@gmail.com>
2025-11-09 20:30:24 +0530

    add some files

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -89,7 +89,7 @@ int main()
     return 0;
 }*/
 
-
+/*
 int main()
 {
     int myNumbers[] = { 5, 3, -6, 10 };
@@ -106,4 +106,171 @@ int main()
         printf("%d\n", myNumbers[i]);
     }
 
-}
+}*/
+
+
+
+
+// leet code
+/*
+int main()
+{
+    int num1[] = {1, 2, 3, 0, 0, 0};
+    int num2[] = {2, 5, 6};
+
+    int output[] = {num1[0], num1[1], num2[0], num1[2], num2[1], num2[2]};
+
+    int elements = sizeof(output) / sizeof(output[0]);
+    int i;
+
+    for(i = 0; i < elements; i++) {
+        printf("%d ", output[i]);
+    }
+}*/
+
+
+// array total sub marks score.
+/*
+int main()
+{
+    int marks[5], i, total = 0;
+    float percentage;
+
+    printf("Enter all sub marks. \n");
+
+    for(i = 0; i <= 4; i++) {
+        printf("Enter marks: \n");
+        scanf("%d", &marks[i]);
+    }
+
+    printf("Output \n");
+
+    for(i = 0; i <= 4; i++) {
+        printf("Sub[%d] = %d \n", i+1, marks[i]);
+        total += marks[i];
+    }
+
+    percentage = total/5;
+
+    printf("total marks = %d \n", total);
+    printf("Avg = %.2f \n", percentage);
+
+    return 0;
+}*/
+
+/*
+int main()
+{
+    char yourName[50];
+    int i, total_word; 
+
+    printf("Enter total letter of your name:  ");
+    scanf("%d", &total_word);
+
+    printf("Enter your name (no space): ");
+    scanf("%s", yourName);
+
+    for(i = 0; i < total_word; i++) {
+        printf("Word[%d] = %c\n", i+1, yourName[i]);
+    }
+
+    return 0;
+}*/
+
+
+
+
+/*
+int main()
+{
+    char yourName[50] = "Himanshu Kumar";
+    int i;
+
+    for(i = 0; yourName[i] != '\0'; i++) {
+        
+        if(yourName[i] == ' ') {
+            printf("space at index %d", i);
+        }
+    }
+
+    return 0;
+}*/
+
+// Multidimensional Arrays
+
+/*
+int main()
+{
+    int num[10][10] = {{ 22, 88, 99 }, { 5, 9, 7 }};
+
+    printf("%d", num[0][2]);
+
+    return 0;
+}*/
+
+
+
+// try in loop
+/*
+int main()
+{
+    int matrix[2][3] = { { 99, 57, 45 }, { 76, 90, 34 }, };
+    int i, j;
+
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d\n", matrix[i][j]);
+        }
+    }
+}*/
+
+/*
+int main()
+{
+    int a[5], i, sum = 0;
+
+    for(i = 0; i <= 4; i++)
+    {   
+        printf("Enter elements : ");
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i <= 4; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+
+    // sum
+    for(i = 0; i <= 4; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("Sum of all array elements are : %d", sum);
+}*/
+
+
+// larget element
+/*
+int main()
+{
+    int a[5], i, largest;
+
+    for(i = 0; i <= 4; i++)
+    {
+        printf("Enter elements : ");
+        scanf("%d", &a[i]);
+    }
+
+    largest = a[0];
+    for(i = 0; i < 4; i++)
+    {
+        if (a[i] > a[0])
+        {
+            largest = a[i];
+        }
+        
+    }
+
+    printf("Largest element = %d", largest);
+}*/
---
+++ functions/functions.c
@@ -0,0 +1,193 @@
+// functions
+
+// create a function
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/*
+
+void myFn()
+{
+    printf("Hello\n");
+}
+
+void sumOfTwoNo()
+{
+    int num1 = 5;
+    int num2 = 4;
+    int sum = num1 + num2;
+
+    printf("%d\n", sum);
+}
+
+// parameters
+void fullName(char name[])
+{
+    printf("My name is %s\n", name);
+}
+
+// sum of two no
+void sum(int num1, int num2)
+{
+    int sum = num1 + num2;
+    printf("The sum of %d and %d is %d", num1, num2, sum);
+}*/
+
+/*
+int factorial(int n);
+
+int main()
+{
+    printf("Factorial of 5 is : %d", factorial(5));
+    return 0;
+}
+
+int factorial(int n) {
+    if(n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+}*/
+
+/*
+int x = 5;
+
+int main()
+{
+    printf("%d", x);
+}*/
+
+// function decleration
+
+/*
+void print();
+
+int sum(int x, int y);
+
+int main()
+{
+    print();
+    return 0;
+}
+
+int sum(int x, int y)
+{
+    return x + y;
+}
+
+void print()
+{
+    printf("hello!");
+    int result = sum(55, 10);
+    printf("\nsum = %d", result);
+}
+*/
+
+// round a number
+/*
+int main()
+{
+    printf("%f\n", ceil(1.1));
+    printf("%f\n", floor(1.8));
+    printf("%f\n", sqrt(16));
+}*/
+
+// inline function
+/*
+static inline int square(int x)
+{
+    return x * x;
+}
+
+int main()
+{
+    printf("square = %d", square(7));
+    return 0;
+}*/
+
+// recursion self call
+/*
+int factor(int x);
+int countDown(int n);
+
+int main()
+{
+    printf("%d", factor(3));
+    printf("%d", countDown(5));
+    return 0;
+}
+
+int factor(int x)
+{
+    if (x > 0)
+    {
+        return x * factor(x - 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int countDown(int n)
+{
+    if (n > 0)
+    {
+        printf("%d", n);
+        countDown(n - 1);
+    }
+
+    return 0;
+}*/
+
+// function pointer. A function pointer is like a normal pointer, but instead of pointing to a variable, it points to a function.
+
+// ex :
+/*
+int add(int a, int b) {
+    return a + b;
+}
+
+void morning() { printf("Good Morning!!\n"); }
+void evening() { printf("Good evening!!\n"); }
+
+void greet(void (*fun)()) {
+    fun();
+}
+
+int main()
+{
+    int (*sum)(int, int) = add;
+    int result = sum(5, 5);
+    greet(morning);
+    greet(evening);
+    printf("sum = %d", result);
+    return 0;
+}*/
+
+// call back function
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int main()
+{
+    int numbers[] = {5, 2, 9, 1, 7};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    qsort(numbers, size, sizeof(int), compare);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+    return 0;
+}
